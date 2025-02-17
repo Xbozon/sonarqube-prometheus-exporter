9ae05b2fa577210e57ba01cc17873a4f6df0f3c4
etolkachev <etolkachev@adsterra.com>
2025-02-17 12:40:00 +0900

    Add caching and background updates

--- core/prometheus_exporter.py
+++ core/prometheus_exporter.py
@@ -1,25 +1,57 @@
+import time
+import threading
+import logging
 from prometheus_client.core import GaugeMetricFamily
 import prometheus_client as prom
-import time
 from sonarqube_exporter import get_all_projects_with_metrics
 
 class CustomSonarExporter:
+    def __init__(self, update_interval=60):
+        """
+        Initializes the exporter.
+        :param update_interval: How often (in seconds) to update the cache from the SonarQube API.
+        """
+        self.cached_projects = []
+        self.lock = threading.Lock()
+        self.update_interval = update_interval
 
-    def __init__(self):
-        pass
+        thread = threading.Thread(target=self.update_cache, daemon=True)
+        thread.start()
+
+    def update_cache(self):
+        """Background loop that periodically updates the cache."""
+        while True:
+            try:
+                projects = get_all_projects_with_metrics()
+                with self.lock:
+                    self.cached_projects = projects
+                logging.info("Successfully updated SonarQube metrics cache.")
+            except Exception as e:
+                logging.exception("Error updating SonarQube metrics cache: %s", e)
+            time.sleep(self.update_interval)
 
     def collect(self):
-        projects = get_all_projects_with_metrics()
+        """
+        Called by Prometheus to collect metrics.
+        Uses the cached projects data.
+        """
+        with self.lock:
+            projects = list(self.cached_projects)  # Shallow copy of the cache
+
+        if not projects:
+            try:
+                projects = get_all_projects_with_metrics()
+            except Exception as e:
+                logging.exception("Error fetching SonarQube metrics directly: %s", e)
+                return
 
         for project in projects:
             for metric in project.metrics:
                 label_list = ['id', 'key', 'name']
-                label_values = []
+                label_values = [project.id, project.key, project.name]
                 value_to_set = None
 
-                label_values.append(project.id)
-                label_values.append(project.key)
-                label_values.append(project.name)
+                # Process each metric's values.
                 for metric_value in metric.values:
                     if metric_value[0] == 'value':
                         value_to_set = metric_value[1]
@@ -32,17 +64,17 @@ class CustomSonarExporter:
                     documentation=metric.description,
                     labels=label_list
                 )
-
-                gauge.add_metric(
-                    labels=label_values,
-                    value=value_to_set
-                )
+                gauge.add_metric(labels=label_values, value=value_to_set)
                 yield gauge
 
 if __name__ == "__main__":
-    custom_exporter = CustomSonarExporter()
+    logging.basicConfig(level=logging.INFO)
+
+    custom_exporter = CustomSonarExporter(update_interval=60)  # Update cache every 60 seconds.
     prom.REGISTRY.register(custom_exporter)
     prom.start_http_server(9120)
+    logging.info("SonarQube exporter is running on port 9120.")
 
+    # Keep 
     while True:
         time.sleep(2)
--- core/sonarqube_exporter.py
+++ core/sonarqube_exporter.py
@@ -23,16 +23,6 @@ class SonarExporter:
     def get_all_projects(self):
         return self._request(endpoint='api/components/search_projects?filter=tags=sonarqube-exporter')
 
-    def get_all_metrics(self):
-        response = self._request(endpoint='api/metrics/search')
-        metrics_list = response['metrics']
-        page_num = 1
-        while len(metrics_list) < response["total"]:
-            page_num += 1
-            response = self._request(endpoint=f'api/metrics/search?p={page_num}')
-            metrics_list.extend(response["metrics"])
-        return metrics_list
-
     def get_measures_component(self, component_key, metric_key):
         return self._request(endpoint="api/measures/component?component={}&metricKeys={}".format(component_key, metric_key))
 
@@ -69,24 +59,26 @@ class Project:
     def organization(self, value):
         self._organization = value
 
-    def organize_measures(self, metrics : list):
+    def organize_measures(self, metrics: list):
         metric_obj_list = []
-        for metric in self.metrics['component']['measures']:
-            if 'metric' in metric:
+        for measure in self.metrics['component']['measures']:
+            if 'metric' in measure:
                 m = Metric()
-                tuple_list = []
+                # Set basic properties from our defined metrics
                 for metric_obj in metrics:
-                    if metric_obj.key == metric['metric']:
+                    if metric_obj.key == measure['metric']:
                         m.key = metric_obj.key
                         m.description = metric_obj.description
                         m.domain = metric_obj.domain
-                for met_tuples in self.transform_object_in_list_tuple(metric):
-                    if met_tuples[0] == 'metric':
-                        m.key = met_tuples[1]
+                # Convert measure data into tuple format
+                tuple_list = []
+                for met_tuple in self.transform_object_in_list_tuple(measure):
+                    if met_tuple[0] == 'metric':
+                        m.key = met_tuple[1]
                     else:
-                        tuple_list.append(met_tuples)
+                        tuple_list.append(met_tuple)
                 m.values = tuple_list
-            metric_obj_list.append(m)
+                metric_obj_list.append(m)
         self.metrics = metric_obj_list
 
     def transform_object_in_list_tuple(self, metric_object):
@@ -99,6 +91,7 @@ class Project:
                 obj_tuple = (str(item), str(metric_object[item]))
                 object_list_tuples.append(obj_tuple)
         return object_list_tuples
+
 
 class Metric:
 
@@ -140,35 +133,27 @@ class Metric:
     def domain(self, value):
         self._domain = value
 
+
 def get_all_projects_with_metrics():
     projects = []
-    metrics = []
-
     client = SonarExporter(CONF.sonar_user, CONF.sonar_password)
     all_projects = client.get_all_projects()
-    all_metrics = client.get_all_metrics()
 
-    for metric in all_metrics:
+    metrics_keys = "code_smells,bugs,vulnerabilities"
+
+    metrics = []
+    for key, description in [("code_smells", "Code Smells"),
+                             ("bugs", "Bugs"),
+                             ("vulnerabilities", "Vulnerabilities")]:
         m = Metric()
-        for item in CONF.supported_keys:
-            if 'domain' in metric and metric['domain'] in item['domain']:
-                if 'key' in metric and metric['key'] in item['keys']:
-                    m.key = metric['key']
-                    m.domain = metric['domain']
-                    if 'description' in metric:
-                        m.description = metric['description']
-                    metrics.append(m)
-
-    metrics_comma_separated = str()
-    for metric in metrics:
-        if metric.description:
-            metrics_comma_separated = "{},{}".format(metric.key, metrics_comma_separated)
+        m.key = key
+        m.description = description
+        metrics.append(m)
 
     for project in all_projects['components']:
         p = Project(identifier=project['key'], key=project['key'])
         p.name = project['name']
-        #p.organization = project['organization']
-        p.metrics = client.get_measures_component(component_key=p.key, metric_key=metrics_comma_separated)
+        p.metrics = client.get_measures_component(component_key=p.key, metric_key=metrics_keys)
         p.organize_measures(metrics)
         projects.append(p)
 
